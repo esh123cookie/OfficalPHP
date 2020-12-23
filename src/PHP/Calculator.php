4f3c0d07fd184bbca4e8f7166970aba32bc91c43
esh123cookie <52058925+esh123cookie@users.noreply.github.com>
2020-12-23 00:00:09 -0500

    Create Calculator.php

---
+++ src/PHP/Calculator.php
@@ -0,0 +1,37 @@
+<?php
+
+   function square($int) {
+        $square = $int * $int;
+        return $square;
+    }
+    
+    function root($int) { 
+        $root = $int / $int;
+        return $root;
+    }
+    
+    function divide($firstInt, $secondInt) { 
+        $divide = $firstInt / $secondInt;
+        return $divide;
+    }
+    
+    function multiply($firstInt, $seoondInt) {
+        $multiply = $firstInt * $seoondInt;
+        return $multiply;
+    } 
+    
+    function calculate(int $ans) { 
+        echo($ans);
+    }
+    
+    $array = array(
+     //   calculate(multiply(multiply(50, 5), divide(29, 5))),
+     //   calculate(multiply(multiply(25, 5), divide(452, 5)))
+    );
+    
+    foreach($array as $answer) { 
+    }
+    
+    echo(calculate(multiply(5, divide(98, root(300)))));
+    
+}
