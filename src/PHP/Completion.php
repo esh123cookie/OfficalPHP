fc4afb842bacf42b857e9534596deecc1719896c
esh123cookie <52058925+esh123cookie@users.noreply.github.com>
2020-12-22 23:58:58 -0500

    Create Completion.php

---
+++ src/PHP/Completion.php
@@ -0,0 +1,20 @@
+<?php 
+    
+    function getTutorial() {
+        return true;
+    }
+    
+    function isComplete() : string{
+	    return getTutorial() ? true : false;
+    }
+    
+    if(isComplete()) { 
+        
+       echo("done");
+       echo(getTutorial());
+    }else{ 
+       echo("notCompleted tutorial");
+       
+    }
+    
+}
