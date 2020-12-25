53fc078890a177efe297ea7297d3114c4be5b943
esh123cookie <52058925+esh123cookie@users.noreply.github.com>
2020-12-25 12:06:24 -0500

    Update Calculator.php

--- src/PHP/Calculator.php
+++ src/PHP/Calculator.php
@@ -22,8 +22,8 @@ class Calculator {
         return $multiply;
     } 
     
-    function calculate(int $ans) { 
-        echo($ans);
+    function calculate(string $message, int $ans) { 
+        echo($message . ": " . $ans);
     }
     
     $array = array(
@@ -35,4 +35,56 @@ class Calculator {
     
          echo(calculate(multiply(5, divide(98, root(300)))));
     }
+   
+    //time 
+   
+    function time_1(int $t1) { 
+       
+      
+    function time_value() { 
+       if($t1 >= 0 and >= 0) { 
+          $time = (time_1() - time_2());
+          return $time;
+       }else{
+          return null;
+       }
+    }
+       
+    //distance 
+    function distance(int $t, int $v1, $v2) { //delta form for velocity and time
+       $time = (time_value());
+       if($v2 = null and $time >= 0 and $v1 >= 0 and $v2 >= 0) { 
+          $acceleration = (($v1 - $v2) / $time);
+          $a = (multiply($v1, $time));
+          $b = (multiply(sqaure($time), sqaure($acceleration)));
+          $c = (divide($b, 2));
+          calculate($a + $c);
+       }
+       //if all 3 are present
+       if($v1 >= 0 and $v2 >= 0 and $time >= 0) { 
+          $a = ($v1 + $v2);
+          calculate((divide($a, 2)) * $time);
+       }
+    }
+   
+   
+    /* 
+    physics 
+    */
+   
+    function kinematics_calculator($distance, $v1, $v2, $time) { 
+       $velocity = ($v1 + $v2); //stored if required
+       
+       if($time >= 0) {
+          $acceleration = (($v1 - $v2) / $time);
+       }
+           
+       if($distance == null) { 
+          calculate("Distance", distance($v1, $v2, $time));
+       }
+       
+       if($velocity == null) { 
+          calculate("
+       
+       
 }
